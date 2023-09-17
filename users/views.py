aaa15472593b3effb5295516d62bf69b36fe87df
zaferberkay <zaferberkay1@gmail.com>
2023-09-17 14:43:33 +0300

    register user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class=RegisterSerializer
+    
+
+
