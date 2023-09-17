139fe818a32d0b0e2b92adcfe1bcbfe0390ca412
zaferberkay <zaferberkay1@gmail.com>
2023-09-17 15:35:34 +0300

    signals

---
+++ users/signals.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+#! püf noktası app.py'a refresh ettiğinde tetiklensin diye import ettimemiz gerekiyor
